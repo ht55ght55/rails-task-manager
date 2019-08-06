4fba61df9bdfe45d85b6ea1b1968be8fe1b8007c
James Hart <jhart2083@gmail.com>
2019-08-06 14:29:29 +0200

    make create route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,22 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Tasks.find
+    @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    console
+    # @task = Task.new
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'task/:id/edit', to: 'tasks#edit'
+
+
 end
